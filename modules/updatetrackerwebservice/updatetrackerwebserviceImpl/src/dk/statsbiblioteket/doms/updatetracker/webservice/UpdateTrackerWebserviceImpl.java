b9cdb93a834a487be3cc6760e542c714b70689bf
blekinge <blekinge@31bcc958-5192-48e2-8e17-0a6350180d47>
2010-09-24 12:37:43 +0000

    Added a method

--- modules/updatetrackerwebservice/updatetrackerwebserviceImpl/src/dk/statsbiblioteket/doms/updatetracker/webservice/UpdateTrackerWebserviceImpl.java
+++ modules/updatetrackerwebservice/updatetrackerwebserviceImpl/src/dk/statsbiblioteket/doms/updatetracker/webservice/UpdateTrackerWebserviceImpl.java
@@ -10,53 +10,41 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
-/* $Id$
- * $Revision$
- * $Date$
- * $Author$
- *
- * The DOMS project.
- * Copyright (C) 2007-2009  The State and University Library
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-@WebService(endpointInterface = "dk.statsbiblioteket.doms.updatetracker.webservice.UpdateTrackerWebservice")
-public class UpdateTrackerWebserviceImpl implements UpdateTrackerWebservice{
 
-    /**
-     * TODO javadoc
-     *
-     * @param collectionPid
-     * @param entryCMPid
-     * @param beginTime
-     * @return returns java.util.List<dk.statsbiblioteket.doms.updatetracker.webservice.PidDatePidPid>
-     *
-     * @throws MethodFailedException
-     * @throws InvalidCredentialsException
-     *
-     */
-    public List<PidDatePidPid> listObjectsChangedSince(
-            @WebParam(name = "collectionPid", targetNamespace = "")
-            String collectionPid,
-            @WebParam(name = "entryCMPid", targetNamespace = "")
-            String entryCMPid,
+/* $Id$
+* $Revision$
+* $Date$
+* $Author$
+*
+* The DOMS project.
+* Copyright (C) 2007-2009  The State and University Library
+*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+@WebService(endpointInterface = "dk.statsbiblioteket.doms.updatetracker.webservice.UpdateTrackerWebservice")
+public class UpdateTrackerWebserviceImpl implements UpdateTrackerWebservice {
+
+       public List<PidDatePidPid> listObjectsChangedSince(
+            @WebParam(name = "collectionPid", targetNamespace = "") String s,
+            @WebParam(name = "entryCMPid", targetNamespace = "") String s1,
+            @WebParam(name = "viewAngle", targetNamespace = "") String s2,
             @WebParam(name = "beginTime", targetNamespace = "")
-            XMLGregorianCalendar beginTime)
+            XMLGregorianCalendar xmlGregorianCalendar)
             throws InvalidCredentialsException, MethodFailedException {
 
         List<PidDatePidPid> result = new ArrayList<PidDatePidPid>();
@@ -91,5 +79,13 @@ public class UpdateTrackerWebserviceImpl implements UpdateTrackerWebservice{
         result.add(objectThatChanged);
 
         return result;
+    }
+
+    public XMLGregorianCalendar getLatestModificationTime(@WebParam(
+            name = "getLatestModificationTime",
+            targetNamespace = "http://updatetracker.doms.statsbiblioteket.dk/",
+            partName = "parameters") PidPidAngle pidPidAngle)
+            throws InvalidCredentialsException, MethodFailedException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
