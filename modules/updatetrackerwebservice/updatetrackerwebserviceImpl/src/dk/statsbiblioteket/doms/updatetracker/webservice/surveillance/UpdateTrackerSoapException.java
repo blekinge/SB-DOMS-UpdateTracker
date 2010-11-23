c17334dbf322e4186022470f574c24ede21c0421
blekinge <blekinge@31bcc958-5192-48e2-8e17-0a6350180d47>
2010-11-23 16:56:05 +0000

    Cutting new version

---
+++ modules/updatetrackerwebservice/updatetrackerwebserviceImpl/src/dk/statsbiblioteket/doms/updatetracker/webservice/surveillance/UpdateTrackerSoapException.java
@@ -0,0 +1,11 @@
+package dk.statsbiblioteket.doms.updatetracker.webservice.surveillance;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: abr
+ * Date: Nov 23, 2010
+ * Time: 4:13:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UpdateTrackerSoapException extends Exception {
+}
